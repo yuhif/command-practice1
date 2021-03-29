8482ba728c216392bca4b948a23579d81fd10cae
fujikawa yuhi <y.fujikawa.sys20@morijyobi.ac.jp>
2021-03-29 12:07:08 +0900

    bmiを2回表示するように変更

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import calc_bmi
 h = int(input("身長："))
 w = int(input("体重："))
 
-print(f"bmiは{calc_bmi.get_bmi(h, w)}です。")
+bmi = calc_bmi.get_bmi(h, w)
+print(f"bmiは{bmi}です。")
+print(f"bmiは{bmi}です。")
